2f8913dae40c6df9124a036f6f0a6c6125d3ff1b
yela12mm <yelainesmartinez9@gmail.com>
2025-06-07 23:06:59 -0500

    modificacion de estructura main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'feature/home/presentation/views/loading.dart';
+import 'feature/home/presentation/views/success.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,70 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-        return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home:Scaffold(
-      body: Stack(
-        children: [
-          // Fondo con imagen
-          SizedBox.expand(
-            child: Image.asset(
-              'assets/fondo.jpeg',
-              fit: BoxFit.cover,
-            ),
-          ),
-          // Capa oscura encima para mejor contraste
-          Container(
-            color: Colors.black.withOpacity(0.5),
-          ),
-          // Contenido
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Bienvenido',
-
-                 
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),                Text(
-                  
-
-                  ' a CineApp',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                
-                const SizedBox(height: 28),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 252, 206, 57), // Amarillo
-                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/home'); // Ruta a Success
-                  },
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(color: Colors.black, fontSize: 18,fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    )
-    );
-  }
+    return MaterialApp(
+      home: Loading()
+      );
+   }
 }
